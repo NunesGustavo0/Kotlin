9343d6b448dd5c0a214e2f1f4cffea11ac6f39ec
Gustavo Nunes <gustavo.nunes0379@gmail.com>
2025-08-12 17:02:06 -0400

    Create quest1.kts

---
+++ lista1/quest1.kts
@@ -0,0 +1,10 @@
+fun main(){
+    val nome = "Gustavo Nunes de Oliveira"
+    var idade = 19
+    
+    println("Meu nome é $nome e tenho $idade anos")
+    
+    idade += 1
+    
+    println("Agora tenho $idade")
+}
