b3f4947c4b5f81d41de5d81f3ac854279d209f31
Gustavo Nunes <gustavo.nunes0379@gmail.com>
2025-08-13 02:09:48 -0400

    Update questao2.kt

--- lista1/questao2.kt
+++ lista1/questao2.kt
@@ -1,6 +1,15 @@
 fun ehPar(numero: Int): Boolean = if((numero % 2) == 0) true else false
 
+fun String.inverter(): String{
+    var result: String = ""
+    for(i in this.length-1 downTo 0){
+        result += this[i]
+    }
+    return result
+}
+
 fun main(){
     println(ehPar(92))
+    val palavra = "Gustavo"
+    println(palavra.inverter())
 }
-#Apenas questão 2.2 falta
