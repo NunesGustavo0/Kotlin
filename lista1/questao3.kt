c746fe72ad0683d1024c5f22efd838da9964eb43
Gustavo Nunes <gustavo.nunes0379@gmail.com>
2025-08-13 00:55:47 -0400

    Create questao3.kt

---
+++ lista1/questao3.kt
@@ -0,0 +1,19 @@
+fun comprimentoOuZero(comprimento: String?): Int = comprimento?.length ?:0
+
+fun descreveEndereco(rua: String?, numero: Int?, cidade: String?): String{
+    val ruaNova: String = rua ?: "Não informado"
+    val numeroNovo = numero ?: "Não informado"
+    val cidadeNova = cidade ?: "Não informado"
+
+    return "Rua $ruaNova, N* $numeroNovo, $cidadeNova"
+}
+
+fun main(){
+    println(comprimentoOuZero(""))
+    println(comprimentoOuZero("Gustavo"))
+
+    println(descreveEndereco("Albano melo", 16, "Manaus"))
+    println(descreveEndereco(null, 16, "Manaus"))
+    println(descreveEndereco("Albano melo", null, "Manaus"))
+    println(descreveEndereco("Albano melo", 16, null))
+}
